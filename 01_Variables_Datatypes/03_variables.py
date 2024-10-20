88318a45d80d1989e34ea6430707a7163c5ee2ef
ashishshres <ashishshr27@gmail.com>
2024-10-20 19:03:48 +0545

    variables

---
+++ 01_Variables_Datatypes/03_variables.py
@@ -0,0 +1,8 @@
+# Variables
+username = "ashish321"
+email = "ashish@example.com"
+
+# Your username is: ashish321
+print("Your username is:", username)
+# Your email is:  ashish@example.com
+print("Your email is: ", email)
